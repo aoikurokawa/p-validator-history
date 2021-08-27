0469b8e317a609020404133ea0adee9caa3c0c78
Aoi <ht.so.ik0102@gmail.com>
2021-08-27 21:21:37 +0700

    change the handler of postavailability

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/Aoi020608/bookings/pkg/config"
@@ -66,7 +67,11 @@ func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
 }
 
 func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Posted to search availability"))
+	start := r.Form.Get("start")
+	end := r.Form.Get("end")
+
+	w.Write([]byte(fmt.Sprintf("start date is %s and end date is %s", start, end)))
+
 }
 
 func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
