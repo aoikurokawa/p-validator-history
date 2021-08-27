e924dee18d3aa1fe12269006462c5cbdbf3d3038
Aoi <ht.so.ik0102@gmail.com>
2021-08-27 19:52:45 +0700

    add contact page

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -20,7 +20,8 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/about", handlers.Repo.About)
 	mux.Get("/generals-quarters", handlers.Repo.Generals)
 	mux.Get("/majors-suite", handlers.Repo.Majors)
-	mux.Get("/search-availability", handlers.Repo.About)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Get("/contact", handlers.Repo.Contact)
 
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
 
