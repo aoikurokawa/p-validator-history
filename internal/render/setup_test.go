9b37962bf9112f2b8e6f8061541b20d93aacb894
Aoi <ht.so.ik0102@gmail.com>
2021-08-29 15:55:46 +0700

    change render_test.go

---
+++ internal/render/setup_test.go
@@ -0,0 +1,35 @@
+package render
+
+import (
+	"encoding/gob"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/Aoi020608/bookings/internal/config"
+	"github.com/Aoi020608/bookings/internal/models"
+	"github.com/alexedwards/scs/v2"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+	// session
+	gob.Register(models.Reservation{})
+	// change this to true when in production
+	testApp.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false
+
+	testApp.Session = session
+
+	app = &testApp
+
+	os.Exit(m.Run())
+}
