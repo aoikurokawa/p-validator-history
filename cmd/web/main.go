7faad9b61e1f1958ef29b363587541e37167b0b0
Aoi <ht.so.ik0102@gmail.com>
2021-08-29 09:21:22 +0700

    check summary

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,6 +9,7 @@ import (
 
 	"github.com/Aoi020608/bookings/internal/config"
 	"github.com/Aoi020608/bookings/internal/handlers"
+	"github.com/Aoi020608/bookings/internal/models"
 	"github.com/Aoi020608/bookings/internal/render"
 	"github.com/alexedwards/scs/v2"
 )
@@ -19,6 +21,8 @@ var app config.AppConfig
 var session *scs.SessionManager
 
 func main() {
+	// session
+	gob.Register(models.Reservation{})
 	// change this to true when in production
 	app.InProduction = false
 
