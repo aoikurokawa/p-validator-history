00e9aa5738bb6081e20effcf4be47d766b0712d6
Aoi <ht.so.ik0102@gmail.com>
2021-08-29 08:57:14 +0700

    add serside validation

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -80,7 +80,10 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 
 	form := forms.New(r.PostForm)
 
-	form.Has("first_name", r)
+	form.Required("first_name", "last_name", "email", "phone")
+	form.MinLength("first_name", 3, r)
+	form.IsEmail("email")
+
 	if !form.Valid() {
 		data := make(map[string]interface{})
 		data["reservation"] = reservation
