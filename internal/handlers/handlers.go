c8ee7649f70286ba4b302a07266bbf5833b69647
Aoi <ht.so.ik0102@gmail.com>
2021-09-01 20:24:00 +0700

    create insert function for reservation

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/Aoi020608/bookings/internal/config"
 	"github.com/Aoi020608/bookings/internal/driver"
@@ -67,11 +69,35 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	sd := r.Form.Get("start_date")
+	ed := r.Form.Get("end_date")
+
+	// 2020-01-01 -> 01/02 03:04:05PM '06 -0700
+
+	layout := "2006-01-02"
+	startDate, err := time.Parse(layout, sd)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
+	endDate, err := time.Parse(layout, ed)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
+	roomID, nil := strconv.Atoi(r.Form.Get("room_id"))
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
 	reservation := models.Reservation{
 		FirstName: r.Form.Get("first_name"),
 		LastName:  r.Form.Get("last_name"),
 		Email:     r.Form.Get("email"),
 		Phone:     r.Form.Get("phone"),
+		StartDate: startDate,
+		EndDate:   endDate,
+		RoomID:    roomID,
 	}
 
 	form := forms.New(r.PostForm)
@@ -89,6 +115,10 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 			Data: data,
 		})
 		return
+	}
+	err = m.DB.InsertReservation(reservation)
+	if err != nil {
+		helpers.ServerError(w, err)
 	}
 
 	m.App.Session.Put(r.Context(), "reservation", reservation)
--- internal/repository/dbrepo/postgress.go
+++ internal/repository/dbrepo/postgress.go
@@ -1,11 +1,42 @@
 package dbrepo
 
-import "github.com/Aoi020608/bookings/internal/models"
+import (
+	"context"
+	"time"
+
+	"github.com/Aoi020608/bookings/internal/models"
+)
 
 func (m *postgressDBRepo) AllUsers() bool {
 	return true
 }
 
+// insert reservation inserts a reservation into the database
 func (m *postgressDBRepo) InsertReservation(res models.Reservation) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `insert into reservations (first_name, last_name, email, phone, start_date 
+			end_date, room_id, created_at, updated_at)
+			values($1, $2, $3, $4, $5, $6, $7, $8, $9)`
+
+	_, err := m.DB.ExecContext(
+		ctx,
+		stmt,
+		res.FirstName,
+		res.LastName,
+		res.Email,
+		res, res.Phone,
+		res.StartDate,
+		res.EndDate,
+		res.RoomID,
+		res.CreatedAt,
+		time.Now(),
+		time.Now(),
+	)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
