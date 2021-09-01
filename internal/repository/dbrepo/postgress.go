5052d945fd4a1b68c830b13f5d1775c5ec5a7ec0
Aoi <ht.so.ik0102@gmail.com>
2021-09-01 19:58:40 +0700

    change postgress.go

--- internal/repository/dbrepo/postgress.go
+++ internal/repository/dbrepo/postgress.go
@@ -1,5 +1,11 @@
 package dbrepo
 
+import "github.com/Aoi020608/bookings/internal/models"
+
 func (m *postgressDBRepo) AllUsers() bool {
 	return true
+}
+
+func (m *postgressDBRepo) InsertReservation(res models.Reservation) error {
+	return nil
 }
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,5 +1,9 @@
 package repository
 
+import "github.com/Aoi020608/bookings/internal/models"
+
 type DatabaseRepo interface {
 	AllUsers() bool
+
+	InsertReservation(res models.Reservation) error
 }
