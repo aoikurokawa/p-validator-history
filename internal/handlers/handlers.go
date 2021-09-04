887961c3b04d564c6a578d86db851fd19b62eb40
Aoi <ht.so.ik0102@gmail.com>
2021-09-04 21:05:03 +0700

    add delete button and function

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -510,3 +510,13 @@ func (m *Repository) AdminShowPostReservation(w http.ResponseWriter, r *http.Req
 func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Request) {
 	render.Template(w, r, "admin-reservations-calendar.page.html", &models.TemplateData{})
 }
+
+func (m *Repository) AdminProcessReservation(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(chi.URLParam(r, "id"))
+	src := chi.URLParam(r, "src")
+
+	_ = m.DB.UpdateProcessForReservation(id, 1)
+	m.App.Session.Put(r.Context(), "flash", "Reservation marked as processed")
+
+	http.Redirect(w, r, fmt.Sprintf("/admin/reservations-%s", src), http.StatusSeeOther)
+}
