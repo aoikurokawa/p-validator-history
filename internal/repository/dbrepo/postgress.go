aecda3540f11a313dc95852169ff6691e4d907c9
Aoi <ht.so.ik0102@gmail.com>
2021-09-04 20:00:21 +0700

    change update query

--- internal/repository/dbrepo/postgress.go
+++ internal/repository/dbrepo/postgress.go
@@ -405,6 +405,8 @@ func (m *postgressDBRepo) UpdateReservation(u models.Reservation) error {
 			reservations 
 		set 
 			first_name = $1, last_name = $2, email = $3, phone = $4, updated_at = $5 
+		where
+			id = $6
 	`
 
 	_, err := m.DB.ExecContext(ctx, query,
@@ -413,6 +415,7 @@ func (m *postgressDBRepo) UpdateReservation(u models.Reservation) error {
 		u.Email,
 		u.Phone,
 		time.Now(),
+		u.ID,
 	)
 
 	if err != nil {
