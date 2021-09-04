3acb0581ca19fdafbbeb0aa918f8bd38e1c988b9
Aoi <ht.so.ik0102@gmail.com>
2021-09-04 16:07:53 +0700

    create reservation all page

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -41,12 +41,12 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/user/logout", handlers.Repo.Logout)
 
 	mux.Route("/admin", func(mux chi.Router) {
-		mux.Use(Auth)
+		// mux.Use(Auth)
 		mux.Get("/dashboard", handlers.Repo.AdminDashboard)
 
-		mux.Get("/admin/reservations-new", handlers.Repo.AdminNewReservations)
-		mux.Get("/admin/reservations-all", handlers.Repo.AdminAllReservations)
-		mux.Get("/admin/reservations-calendar", handlers.Repo.AdminReservationsCalendar)
+		mux.Get("/reservations-new", handlers.Repo.AdminNewReservations)
+		mux.Get("/reservations-all", handlers.Repo.AdminAllReservations)
+		mux.Get("/reservations-calendar", handlers.Repo.AdminReservationsCalendar)
 	})
 
 	fileServer := http.FileServer(http.Dir("./static/"))
