97cef8b704527e2f2e5066d6250fb737ef79cf1a
Aoi <ht.so.ik0102@gmail.com>
2021-09-04 11:16:57 +0700

    create logout function

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -38,6 +38,7 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/user/login", handlers.Repo.ShowLogin)
 	mux.Post("/user/login", handlers.Repo.PostLogin)
+	mux.Get("/user/logout", handlers.Repo.Logout)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -397,3 +397,10 @@ func (m *Repository) PostLogin(w http.ResponseWriter, r *http.Request) {
 	m.App.Session.Put(r.Context(), "flash", "login successfully")
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
+
+func (m *Repository) Logout(w http.ResponseWriter, r *http.Request) {
+	_ = m.App.Session.Destroy(r.Context())
+	_ = m.App.Session.RenewToken(r.Context())
+
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+}
