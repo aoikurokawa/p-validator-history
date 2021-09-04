d4dfde3c7f6b6dae517982e2e2ef9c0f578c1cea
Aoi <ht.so.ik0102@gmail.com>
2021-09-04 10:47:29 +0700

    modify template

--- internal/models/templatedata.go
+++ internal/models/templatedata.go
@@ -4,13 +4,14 @@ import "github.com/Aoi020608/bookings/internal/forms"
 
 // TemplateData holds data sent from handlers to tempaltes
 type TemplateData struct {
-	StringMap map[string]string
-	IntMap    map[int]int
-	FloatMap  map[float32]float32
-	Data      map[string]interface{}
-	CSRFToken string
-	Flash     string
-	Warning   string
-	Error     string
-	Form      *forms.Form
+	StringMap       map[string]string
+	IntMap          map[int]int
+	FloatMap        map[float32]float32
+	Data            map[string]interface{}
+	CSRFToken       string
+	Flash           string
+	Warning         string
+	Error           string
+	Form            *forms.Form
+	IsAuthenticated int
 }
