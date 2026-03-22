636e6f9f4761de90ecbd5221f1ea1eb2d07d9aa6
aoikurokawa <ht.so.ik0102@gmail.com>
2026-03-23 03:14:56 +0900

    feat: initialize

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+#![cfg_attr(not(test), no_std)]
+
+use quasar_lang::prelude::*;
+
+declare_id!("CnpTBdVonDQAVUSpHngyPNTizW4Zeb1DFn1iGrvJV9Su");
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    pub payer: &'info mut Signer,
+    pub system_program: &'info Program<System>,
+}
+
+impl<'info> Initialize<'info> {
+    #[inline(always)]
+    pub fn initialize(&self) -> Result<(), ProgramError> {
+        Ok(())
+    }
+}
+
+#[program]
+mod my_program {
+    use super::*;
+
+    #[instruction(discriminator = 0)]
+    pub fn initialize(ctx: Ctx<Initialize>) -> Result<(), ProgramError> {
+        ctx.accounts.initialize()
+    }
+}
+
+#[cfg(test)]
+mod tests;
